5784bae78a6c88b6f13e7e255f092663648ea0de
AlexPNguyen414 <nnguy414@ucr.edu>
2022-02-24 23:37:35 -0800

    add Select_Not class

--- select.hpp
+++ select.hpp
@@ -57,4 +57,21 @@ public:
     }
 };
 
+/* Select_Not */
+class Select_Not : public Select 
+{
+protected:
+    Select* s;
+public:
+    Select_Not(Select* input) :s(input) {}
+    ~Select_Not() { 
+        delete s; 
+    }
+
+    virtual bool select(const Spreadsheet* sheet, int row) const 
+    {
+        return !s->select(sheet, row);
+    }
+};
+
 #endif //__SELECT_HPP__
