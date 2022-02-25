c4b9e1e6c0a62f4a7ec61435e7e76f1fd9c62992
AlexPNguyen414 <nnguy414@ucr.edu>
2022-02-24 22:27:08 -0800

    add print_selection definition

--- spreadsheet.cpp
+++ spreadsheet.cpp
@@ -40,3 +40,32 @@ int Spreadsheet::get_column_by_name(const std::string& name) const
             return i;
     return -1;
 }
+
+void Spreadsheet::print_selection(std::ostream& out) const
+{
+    /* if select points to null, prints everything */ 
+    if (select == nullptr) {
+        for (int i = 0; i < this->data.size(); i++) {   /* rows */ 
+
+            for (int k = 0; k < this->column_names.size(); k++) {   /* columns */
+
+                out << this->cell_data(i, k) << " ";
+            } /* end of k for loop */ 
+            
+            out << std::endl;
+        } /* end of i for loop */ 
+    } /* end of if  */
+    else {
+        for (int i = 0; i < this->data.size(); i++) {   /* rows */ 
+
+            if (this->select->select(this, i)) {    /* calls select function with row i */ 
+                for (int k = 0; k < this->column_names.size(); k++) {   /* columns */
+                    
+                    out << this->cell_data(i, k) << " ";
+                } /* end of k for loop */ 
+                
+                out << std::endl;
+            } /* end of select() if */ 
+        } /* end of i for loop */
+    } /* end of else */ 
+}
